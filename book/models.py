3f90dbf062c715f7cfd1274aa2718c6ab39b87b4
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2022-12-28 11:10:49 +0200

    add book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    class Enum(models.IntegerChoices):
+        HARD = 0
+        SOFT = 1
+    
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    cover = models.BooleanField(choices=Enum.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
