d49b54aac8c4a21a691219664cd75c9ea998b594
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2022-12-31 11:01:11 +0200

    added payment app

--- library_service/settings.py
+++ library_service/settings.py
@@ -154,3 +154,4 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379"
 CELERY_TIMEZONE = "Europe/Kiev"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
